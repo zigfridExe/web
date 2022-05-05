aacfca05ff5e289f793be67e3003bd6a466f1cfd
zigfridExe <zigfrid.exe@gmail.com>
2022-05-04 21:44:03 -0300

    screenshot probblem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
+const { borderRadius } = require('tailwindcss/defaulttheme')
+
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6'
+        },
+        borderRadius: {
+          md: '4px'
         }
       }
     }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')]
 }
